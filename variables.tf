ccc7c0d75da297d3391d1548a17e1e7864d4dfdc
Prem Kumar <113685518+premkumar0482@users.noreply.github.com>
2025-01-02 20:30:46 +0530

    Add files via upload

---
+++ variables.tf
@@ -0,0 +1,45 @@
+variable "aws_region"{
+  default = "ap-south-1"
+}
+
+variable "key_name" {
+  default = "keypair1"
+}
+
+variable "instance_type" {
+  default = "t2.micro"
+}
+
+variable "security_groups" {
+  default = "SG_All"
+}
+
+variable "tag_name" {
+  default = "jdtinstance"
+}
+
+variable "ami_id" {
+  default = "ami-0fd05997b4dff7aac"
+}
+
+variable "versioning" {
+  type = bool
+  default = true
+}
+
+variable "acl" {
+  type =  string 
+  default = "private"
+}
+
+variable "bucket_prefix" {
+  type = string
+  default = "bucketjdt"
+}
+
+variable "tags" {
+  default = {
+    Environment= "DEV"
+    terraform = "true"
+  }
+}
